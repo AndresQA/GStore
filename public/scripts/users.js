a5b45ff08bfb52c36b1b477e887f037e483d40d5
Anivi <anivi98@hotmail.com>
2019-11-29 10:55:52 -0500

    valor carrito

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', function () {
 
+    var valueprice = document.querySelectorAll(".valueprice");
+    var totalrpice = 0;
+
+    valueprice.forEach(viewvalue => {
+        console.log(viewvalue)
+        totalrpice += parseInt((viewvalue.innerHTML).replace("$", ""))
+    });
+
+
+    var totaltotal = document.querySelector(".totalpricevalue");
+
+    totaltotal.innerHTML = "$ "+ totalrpice;
 
 
     var btn = document.querySelector("#buyit");
