8e27d69d333430bfd815fd60b881a0d55b5938cf
anivi <anivi98@hotmail.com>
2019-11-29 11:03:29 -0500

    valor total fixed

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -14,33 +14,5 @@ window.addEventListener('load', function () {
     totaltotal.innerHTML = "$ "+ totalrpice;
 
 
-    var btn = document.querySelector("#buyit");
-
-    btn.addEventListener('click', function () {
-        var nom = document.querySelector('#inp5').value;
-        var ced = document.querySelector('#inp6').value;
-
-        if (nom === "" || ced === "") {
-            alert('llene todos los campos');
-            return;
-        } else {
-            fetch(`/api/vars`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: `nombre=${nom}&cedula=${ced}`
-            }).then(function (respuesta) {
-                return respuesta.text();
-            }).catch(function (error) {
-                console.error(error);
-            }).then(function (mensaje) {
-                console.log(mensaje);
-            });
-
-            window.location.href = "/pago";
-        }
-
-    });
 
 });
